ea84a96d4c69eb5bbfbb0aad9e0a2046fbc9f047
n1ckfg <nick@fox-gieg.com>
2018-06-06 10:50:01 -0400

    run scripts

--- run_openpose.bat
+++ run_openpose.bat
@@ -1,18 +1,20 @@
 @echo off
 
+cd openpose
+
 rem // basic
-rem openpose\bin\OpenPoseDemo.exe
+rem bin\OpenPoseDemo.exe
 
 rem // recording
-openpose\bin\OpenPoseDemo.exe --write_json %~dp0\openpose\record\
+bin\OpenPoseDemo.exe --write_json %~dp0\record\
 
 rem // basic hands and face, ~50% fps vs. body only
-rem openpose\bin\OpenPoseDemo.exe --hand --face
+rem bin\OpenPoseDemo.exe --hand --face
 
 rem // high precision mode, requires > 8GB VRAM
-rem openpose\bin\OpenPoseDemo.exe --net_resolution "1312x736" --scale_number 4 --scale_gap 0.25
+rem bin\OpenPoseDemo.exe --net_resolution "1312x736" --scale_number 4 --scale_gap 0.25
 
 rem // 3D mode, requires > 1 camera
-rem openpose\bin\OpenPoseDemo.exe --flir_camera --3d --number_people_max 1
+rem bin\OpenPoseDemo.exe --flir_camera --3d --number_people_max 1
 
 @pause
--- run_openpose.sh
+++ run_openpose.sh
@@ -1,14 +1,16 @@
+cd openpose
+
 # basic
-#./openpose/build/examples/openpose/openpose.bin
+#./build/examples/openpose/openpose.bin
 
 # recording
-./openpose/build/examples/openpose/openpose.bin --write_json ./openpose/record/
+./build/examples/openpose/openpose.bin --write_json record/
 
 # basic hands and face, ~50% fps vs. body only
-#./openpose/build/examples/openpose/openpose.bin --hand --face
+#./build/examples/openpose/openpose.bin --hand --face
 
 # high precision mode, requires > 8GB VRAM
-#./openpose/build/examples/openpose/openpose.bin --net_resolution "1312x736" --scale_number 4 --scale_gap 0.25
+#./build/examples/openpose/openpose.bin --net_resolution "1312x736" --scale_number 4 --scale_gap 0.25
 
 # 3D mode, requires > 1 camera
-#./openpose/build/examples/openpose/openpose.bin --flir_camera --3d --number_people_max 1
+#./build/examples/openpose/openpose.bin --flir_camera --3d --number_people_max 1
