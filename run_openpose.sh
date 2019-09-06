1ce5a491f955a54462687f8fab93aa337df98856
n1ckfg <nick@fox-gieg.com>
2019-09-06 16:01:56 -0400

    new shell script

--- run_openpose.sh
+++ run_openpose.sh
@@ -1,25 +1,65 @@
-COUNTER=10
-until [  $COUNTER -lt 0 ]; do
-    echo ... $COUNTER ...
-    let COUNTER-=1
-    sleep 1
-done
+#!/bin/bash
 
-echo Launching OpenPose...
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR
 
 cd openpose
 
-# basic
-#./build/examples/openpose/openpose.bin
+PS3='Please enter your choice: '
+options=("Demo" "Record" "Hands/Face" "Record High Precision" "3D")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Demo")
+            echo "Demo" # no recording
+            ./build/examples/openpose/openpose.bin
+            break
+            ;;
+        "Record")
+            echo "Record"
+            ./build/examples/openpose/openpose.bin --write_json record/
+            break
+            ;;
+        "Hands/Face")
+            echo "Hands/Face" # ~50% fps vs. body only
+            ./build/examples/openpose/openpose.bin --hand --face
+            break
+            ;;
+        "Record High Precision")
+            echo "Record High Precision" # requires > 8GB VRAM
+            ./build/examples/openpose/openpose.bin --net_resolution "1312x736" --scale_number 4 --scale_gap 0.25 --write_json record/
+            break
+            ;;
+        "3D")
+            echo "3D" # requires > 1 camera
+            ./build/examples/openpose/openpose.bin --flir_camera --3d --number_people_max 1
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
 
-# recording
-./build/examples/openpose/openpose.bin --write_json record/
 
-# basic hands and face, ~50% fps vs. body only
-#./build/examples/openpose/openpose.bin --hand --face
 
-# high precision mode, requires > 8GB VRAM
-#./build/examples/openpose/openpose.bin --net_resolution "1312x736" --scale_number 4 --scale_gap 0.25
 
-# 3D mode, requires > 1 camera
-#./build/examples/openpose/openpose.bin --flir_camera --3d --number_people_max 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
